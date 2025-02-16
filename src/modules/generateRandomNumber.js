d2eb8848ca4d32181bbd58ea1993bd5c136a2f64
maxmodrr <maxmodrr@gmail.com>
2025-02-16 10:49:51 +0200

    results

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,17 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  let str = '';
+
+  while (str.length !== 4) {
+    const randNum = Math.floor(Math.random() * 9 + 1);
+
+    if (!str.includes(randNum.toString())) {
+      str += randNum;
+    }
+  }
+
+  return +str;
 }
 
 module.exports = {
